4448c2ac46436e5a72ee91fe5e6a3f95659704a6
Ryan Avery <ravery@ucsb.edu>
2020-09-17 22:48:44 -0700

    initial script

---
+++ order.sh
@@ -0,0 +1,15 @@
+# export pl api key if porder doesn't work after using planet init
+export PL_API_KEY=######################
+# create an idlist from an aoi and date range. there are other filters in the porder docs for more complicated orders
+porder idlist --input "/home/rave/kelsey_order/data/AOI_PunjabBurning_poly_simple.geojson" --start "2019-08-26" --end "2019-12-20" --item "PSScene4Band" --asset "analytic_sr" --outfile "/home/rave/kelsey_order/data/idlist.csv" --cmax ".50" --overlap "10" --number 10000000
+# splits idlist into multiple csvs with max order size length. you can make 10 orders of size 500 at a time for a total of 5000 assets being processed at a time
+porder idsplit --idlist "/home/rave/tana-spin/waves/kelsey_order/data/idlist.csv" --lines "500" --local "/home/rave/tana-spin/waves/kelsey_order/data/splitcsvs"
+# get list of csv filenames
+csvs=$(ls /home/rave/tana-spin/waves/kelsey_order/data/splitcsvs/*.csv)
+# this will only work if there's a max of 10 csvs in the splitcsv folder. otherwise too many concurrent requests
+for csv in $csvs; do porder order --name "Punjab Burns {$csv}" --idlist "/home/rave/tana-spin/waves/kelsey_order/data/splitcsvs/{$csv}" --item "PSScene4Band" --boundary "/home/rave/kelsey_order/data/AOI_PunjabBurning_poly_simple.geojson" --bundle "analytic_sr_udm2,analytic_sr" --op clip harmonize email
+; done  
+# get space delimited order links
+orders=$(porder ostate --state success --start 2020-09-16 --end 2020-09-18 | sed -n 5,14p | awk -F "|" '{print $4}')
+# download each order using the async/multiprocessing downloader, each order link is processed sequentially
+for link in $orders; do porder multiproc --url $link --local "/home/rave/tana-spin/waves/kelsey_order/download"; done  
