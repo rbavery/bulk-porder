8a5623de525711a25506dadcea1102c9f3d39d21
Ryan Avery <ravery@ucsb.edu>
2020-09-28 11:33:47 -0700

    updated order

--- order.sh
+++ order.sh
@@ -6,9 +6,10 @@ porder idlist --input "/home/rave/kelsey_order/data/AOI_PunjabBurning_poly_simpl
 porder idsplit --idlist "/home/rave/tana-spin/waves/kelsey_order/data/idlist.csv" --lines "500" --local "/home/rave/tana-spin/waves/kelsey_order/data/splitcsvs"
 # get list of csv filenames
 csvs=$(ls /home/rave/tana-spin/waves/kelsey_order/data/splitcsvs/*.csv)
+# used to pause if too many orders running using while loop
+running=$(porder ostate --state running --start 2020-09-25 --end 2020-09-26 | sed -n 5,14p | awk -F "|" '{print $4}' | wc -l)
 # this will only work if there's a max of 10 csvs in the splitcsv folder. otherwise too many concurrent requests
-for csv in $csvs; do porder order --name "Punjab Burns {$csv}" --idlist "/home/rave/tana-spin/waves/kelsey_order/data/splitcsvs/{$csv}" --item "PSScene4Band" --boundary "/home/rave/kelsey_order/data/AOI_PunjabBurning_poly_simple.geojson" --bundle "analytic_sr_udm2,analytic_sr" --op clip harmonize email
-; done  
+for csv in $csvs; while [ $running -gt 10 ]; do echo "can't order"; sleep 100; running=$(porder ostate --state running --start 2020-09-25 --end 2020-09-26 | sed -n 5,14p | awk -F "|" '{print $4}' | wc -l); done; do porder order --name "kuiseb {$csv}" --idlist "/home/rave/serdp/kuiseb/bulk-porder/splitcsvs-kuiseb/$csv" --item "PSScene4Band" --boundary "/home/rave/serdp/kuiseb/bulk-porder/data/kuiseb.geojson" --bundle "analytic_sr_udm2,analytic_sr" --op clip harmonize email;   done
 # get space delimited order links
 orders=$(porder ostate --state success --start 2020-09-16 --end 2020-09-18 | sed -n 5,14p | awk -F "|" '{print $4}')
 # download each order using the async/multiprocessing downloader, each order link is processed sequentially
